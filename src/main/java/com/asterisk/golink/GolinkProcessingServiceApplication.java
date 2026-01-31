3205b53a77ed796a79e7442e95cc8efae8ab24da
Miguel Sanmartin <miguel.sanmartin.ext@idrica.com>
2026-01-31 19:08:20 +0100

    first commit

---
+++ src/main/java/com/asterisk/golink/GolinkProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.asterisk.golink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolinkProcessingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolinkProcessingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asterisk/golink/GolinkProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asterisk.golink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolinkProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
